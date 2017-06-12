ca8495c5daf88c8f6d82ae285589b32d6fdc11af
Dang Nguyen <dphi999@gmail.com>
2017-06-12 12:04:32 +0700

    Download problems

--- api/upload-problem.js
+++ api/upload-problem.js
@@ -9,7 +9,7 @@ var problemList 		= require('../helpers/upload-problem')
 // Rename submission files so that Themis can parse user's name and problem's name.
 var storage = multer.diskStorage({
 	destination: function(req, file, cb) {
-		cb(null, './data/problems');
+		cb(null, './public/data/problems');
 	},
 });
 
@@ -22,7 +22,8 @@ router.post('/', [ensureAuthorized, upload], function(req, res) {
 	var username = req.body.username;
 	var problemName = req.file.originalname;
 	var topic = req.body.topic;
-	var filePath = path.join(process.cwd(), 'data/problems', req.file.filename);
+	var filePath = path.join('data/problems', req.file.filename);
+
 
 	problemList.addProblem(username, problemName, topic, filePath)
 
